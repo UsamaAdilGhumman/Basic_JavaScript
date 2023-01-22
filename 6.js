c49ebdd247911277d621061470faa428433c6795
Osama Tariq <usama.adil.ghumman@gmail.com>
2023-01-22 15:12:19 +0500

    loops for while do-while

---
+++ 6.js
@@ -0,0 +1,20 @@
+// while loop
+
+// natural num sum 
+// (num*(num+1))/2
+// for loop
+
+// break 
+// continue
+
+// do while loop
+
+let i = 0
+do{
+    console.log("i >>>>> ",i);
+    i++;
+}while(i<9)
+
+console.log("value of i ",i);
+
+
